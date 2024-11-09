f52c1b498923b12580fa39ac7c00a9ce535edd93
mi77888 <sk8102088@gmail.com>
2024-11-09 10:20:51 +0600

    =)

--- plane_user/serializers.py
+++ plane_user/serializers.py
@@ -1,38 +1,43 @@
 from rest_framework import serializers
 from .models import Cart, CartItem, SelectedParts, Component, Plane
 
+# Сериализатор для компонента самолета (деталь, запчасть)
 class ComponentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Component
-        fields = ['id', 'name', 'description', 'stock', 'price', 'category']
+        fields = ['id', 'name', 'description', 'stock', 'price', 'category']  # Включает ID, название, описание, запас, цену и категорию компонента
 
 
+# Сериализатор для выбранных пользователем комплектующих
 class SelectedPartsSerializer(serializers.ModelSerializer):
-    part = ComponentSerializer(read_only=True)
+    part = ComponentSerializer(read_only=True)  # Вложенный сериализатор для отображения данных о компоненте
 
     class Meta:
         model = SelectedParts
-        fields = ['id', 'part', 'quantity']
+        fields = ['id', 'part', 'quantity']  # Включает ID, сам компонент и количество выбранных единиц
 
 
+# Сериализатор для элемента корзины (каждая позиция в корзине)
 class CartItemSerializer(serializers.ModelSerializer):
-    component = ComponentSerializer(read_only=True)
-    selected_part = SelectedPartsSerializer(read_only=True)
+    component = ComponentSerializer(read_only=True)  # Вложенный сериализатор для компонента
+    selected_part = SelectedPartsSerializer(read_only=True)  # Вложенный сериализатор для выбранных частей
 
     class Meta:
         model = CartItem
-        fields = ['id', 'component', 'selected_part', 'quantity']
+        fields = ['id', 'component', 'selected_part', 'quantity']  # ID элемента корзины, компонент, выбранная часть и количество
 
 
+# Сериализатор для корзины пользователя
 class CartSerializer(serializers.ModelSerializer):
-    items = CartItemSerializer(many=True, read_only=True)
+    items = CartItemSerializer(many=True, read_only=True)  # Вложенный сериализатор для списка элементов корзины
 
     class Meta:
         model = Cart
-        fields = ['id', 'user', 'items']
+        fields = ['id', 'user', 'items']  # ID корзины, пользователь и список элементов
 
 
+# Сериализатор для добавления комментариев к модели самолета (если поле 'comment' существует)
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Plane
-        fields = ['id', 'name', 'comment']  # Убедитесь, что поле 'comment' существует в модели Plane
+        fields = ['id', 'name', 'comment']  # Включает ID самолета, его имя и комментарий; убедитесь, что поле 'comment' добавлено в модель Plane
